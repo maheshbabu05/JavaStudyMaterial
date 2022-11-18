35d7025841d2de3506d36f18d67ebded706d2055
Mahesh <maheshbabuece143@gmail.com>
2022-11-17 23:52:41 -0700

    Exception Handle Commti

--- CompleteJava/src/Java_Topics/ExceptionHand.java
+++ CompleteJava/src/Java_Topics/ExceptionHand.java
@@ -3,9 +3,36 @@ package Java_Topics;
 public class ExceptionHand {
 	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Here, Exception handle via finally block using Try and Catch
+		
+		public class arthmeticexp {
+
+            public void main(String[] args) 
+           { 
+               int a = 10, b = 5, c = 5, result; 
+               try { 
+                   result = a / (b - c); 
+                   System.out.println("result" + result); 
+               } 
+                     
+         
+               catch (ArithmeticException e) { 
+                   System.out.println("Exception caught:Division by zero"); 
+               } 
+                               
+              finally { 
+                   System.out.println("final block"); 
+               } 
+              System.out.println("no exe"); 
+               result = a / (b - c); 
+               System.out.println("result" + result); 
+
+           } 
+            
+   }
+
+
 
 	}
 
-}
+
